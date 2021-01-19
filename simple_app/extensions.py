f5485734447752de2321404900f908168041d3fa
bwilson <bwilson@co.clatsop.or.us>
2021-01-19 14:39:02 -0800

    Using factory pattern for app

--- simple_app/extensions.py
+++ simple_app/extensions.py
@@ -1,8 +1,5 @@
 from flask_bootstrap import Bootstrap
 from flask_debugtoolbar import DebugToolbarExtension
-#from flask_celery import Celery
 
 bootstrap = Bootstrap()
 debug_toolbar = DebugToolbarExtension()
-#celery = Celery()
-
